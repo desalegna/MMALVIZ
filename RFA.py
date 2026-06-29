e44a076c0068f9bc15a26a6e519df8a920a985d5
Desalegn Aweke <desbdu@gmail.com>
2026-06-29 22:49:08 +0800

    Update RFA.py

--- RFA.py
+++ RFA.py
@@ -51,13 +51,10 @@ class Config:
     N_QUERY            = 2
     EVAL_EPISODES      = 50
 
-    # MMALVIZ dataset folders nested under data/
-    API_IMAGE_DIR     = os.environ.get(
-        "API_IMAGE_DIR",     os.path.join("data", "api_call_images"))
-    TRAFFIC_IMAGE_DIR = os.environ.get(
-        "TRAFFIC_IMAGE_DIR", os.path.join("data", "network_traffic_images"))
-    RESULTS_DIR       = os.environ.get(
-        "RESULTS_DIR",       os.path.join(SCRIPT_DIR, "results_rfa"))
+    # MMALVIZ dataset
+    API_IMAGE_DIR     = os.environ.get("API_IMAGE_DIR",     os.path.join(SCRIPT_DIR, "api_call_images"))
+    TRAFFIC_IMAGE_DIR = os.environ.get("TRAFFIC_IMAGE_DIR", os.path.join(SCRIPT_DIR, "network_traffic_images"))
+    RESULTS_DIR       = os.environ.get( "RESULTS_DIR",       os.path.join(SCRIPT_DIR, "results_rfa"))
 
 
 POISONING_RATES  = [0.3, 0.5]
